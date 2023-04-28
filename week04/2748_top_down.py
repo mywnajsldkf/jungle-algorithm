ba055ca1e42794505d8d48eb61fe477f9c8ecde3
mywnajsldkf <mywnajsldkf@gmail.com>
2023-04-28 11:00:19 +0900

    [B1] 2748: 피보나치수2(top-down)

---
+++ week04/2748_top_down.py
@@ -0,0 +1,18 @@
+from sys import stdin as s
+import sys
+
+s = open("input.txt", "rt")
+
+n = int(s.readline())
+
+
+dp = [0] * (n+1)
+dp[0] = 0
+dp[1] = 1
+
+def fib(n):
+    for i in range(2, n+1):
+        dp[i] = dp[i-2] + dp[i-1]
+    return dp[n]
+
+print(fib(n))
